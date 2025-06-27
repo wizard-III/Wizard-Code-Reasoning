ab757700c72a295cf75f8e5072ca486831298708
RyanLiu112 <1036433215@qq.com>
2025-06-27 16:11:19 +0800

    Update start_ray.sh

--- tools/start_ray.sh
+++ tools/start_ray.sh
@@ -14,13 +14,38 @@ echo "Worker 节点: ${WORKER_IPS[*]}"
 
 # 启动 Head 节点
 echo "正在启动 Head 节点 ($HEAD_IP)..."
-ray stop && ray start --head --port=6379 --dashboard-port=8265
+export TORCH_NCCL_ENABLE_MONITORING=0
+export NCCL_SHM_DISABLE=1
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
+export NCCL_IB_QPS_PER_CONNECTION=4
+export NCCL_MIN_NCHANNELS=16
+export NCCL_IB_HCA=mlx5
+export NCCL_DEBUG=WARN
+export HYDRA_FULL_ERROR=1
+ray stop
+ray start --head --port=6379 --dashboard-port=8265
 HEAD_ADDRESS="$HEAD_IP:6379"
 
 
 for WORKER_IP in "${WORKER_IPS[@]}"; do
-  (ssh -o ConnectTimeout=10 "$WORKER_IP" "\
-  ray stop && ray start --address=$HEAD_ADDRESS") &
+  ssh -o BatchMode=yes "$WORKER_IP" bash -l <<EOF &
+conda activate archer
+export TORCH_NCCL_ENABLE_MONITORING=0
+export NCCL_SHM_DISABLE=1
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=0
+export NCCL_IB_GID_INDEX=3
+export NCCL_IB_QPS_PER_CONNECTION=4
+export NCCL_MIN_NCHANNELS=16
+export NCCL_IB_HCA=mlx5
+export NCCL_DEBUG=WARN
+export HYDRA_FULL_ERROR=1
+ray stop
+ray start --address="$HEAD_ADDRESS"
+EOF
+
 done
 wait
 
